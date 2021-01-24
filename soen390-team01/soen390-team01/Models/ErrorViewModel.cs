ad5998f0748dc661a277342a908fdd1da709ca97
Juan Hoyos <juanse.jsh@gmail.com>
2021-01-24 01:35:14 -0500

    version change to .Net 5.0

---
+++ soen390-team01/soen390-team01/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace soen390_team01.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ soen390-team01/soen390-team01Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace soen390_team01Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
