cf652ea000b2b107286ec3009e5a07ffc8c370ae
kmarsolais <49344244+kmarsolais@users.noreply.github.com>
2021-01-30 15:43:01 -0500

    Created the abstract Items class

---
+++ soen390-team01/soen390-team01/Models/Items.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace soen390_team01.Models
+{
+    public abstract class Items
+    {
+        protected long ItemId;
+
+        protected abstract string Name { get; set; }
+
+        protected abstract double Price { get; set; }
+
+        protected abstract string Grade { get; set; }
+    }
+}
