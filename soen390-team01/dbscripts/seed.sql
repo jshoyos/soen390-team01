7e03b79b7d56127c27f9a23f111d9a27ae20191a
bakou912 <tom912_a_@hotmail.com>
2021-01-30 19:18:25 -0500

    fixing many-to-many inventory relationships primary keys

--- soen390-team01/dbscripts/seed.sql
+++ soen390-team01/dbscripts/seed.sql
@@ -28,27 +28,11 @@ ALTER TABLE public.bike OWNER TO soen390team01devuser;
 CREATE TABLE public.bike_part (
     bike_id bigint NOT NULL,
     part_id bigint NOT NULL,
-    part_quantity integer NOT NULL,
-    bike_part_id bigint NOT NULL
+    part_quantity integer NOT NULL
 );
 
 
 ALTER TABLE public.bike_part OWNER TO soen390team01devuser;
-
-
-CREATE SEQUENCE public.bike_part_bike_part_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.bike_part_bike_part_id_seq OWNER TO soen390team01devuser;
-
-
-ALTER SEQUENCE public.bike_part_bike_part_id_seq OWNED BY public.bike_part.bike_part_id;
-
 
 
 CREATE TABLE public.inventory (
@@ -132,13 +116,11 @@ CREATE TABLE public.part_material (
 ALTER TABLE public.part_material OWNER TO soen390team01devuser;
 
 
-ALTER TABLE ONLY public.bike_part ALTER COLUMN bike_part_id SET DEFAULT nextval('public.bike_part_bike_part_id_seq'::regclass);
-
 ALTER TABLE ONLY public.inventory ALTER COLUMN inventory_id SET DEFAULT nextval('public.inventory_inventory_id_seq'::regclass);
 
 
 ALTER TABLE ONLY public.bike_part
-    ADD CONSTRAINT bike_part_pkey PRIMARY KEY (bike_part_id);
+    ADD CONSTRAINT bike_part_pkey PRIMARY KEY (bike_id, part_id);
 
 
 ALTER TABLE ONLY public.bike
@@ -153,24 +135,16 @@ ALTER TABLE ONLY public.inventory
     ADD CONSTRAINT inventory_pkey PRIMARY KEY (inventory_id);
 
 
-
 ALTER TABLE ONLY public.material
     ADD CONSTRAINT material_pkey PRIMARY KEY (item_id);
 
 
-
 ALTER TABLE ONLY public.part_material
-    ADD CONSTRAINT part_material_pkey PRIMARY KEY (part_id);
+    ADD CONSTRAINT part_material_pkey PRIMARY KEY (part_id, material_id);
 
 
 ALTER TABLE ONLY public.part
     ADD CONSTRAINT part_pkey PRIMARY KEY (item_id);
-
-
-
-ALTER TABLE ONLY public.part_material
-    ADD CONSTRAINT unique_part_material UNIQUE (part_id) INCLUDE (material_id);
-
 
 
 ALTER TABLE ONLY public.bike_part
