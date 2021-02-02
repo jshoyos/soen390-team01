6d0e15f98217914af2243085b8f1d5dbb2618d34
noahfreger <50425626+noahfreger@users.noreply.github.com>
2021-02-01 20:23:32 -0500

    added files

---
+++ soen390-team01/soen390-team01/Models/InventoryModel.cs
@@ -0,0 +1,16 @@
+using System;
+using soen390_team01.Data.Entities;
+
+namespace soen390_team01.Models
+{
+    public class InventoryModel:Inventory
+    {
+ 
+        public void getAllBikes()
+        {
+            
+
+        }
+       
+    }
+}
