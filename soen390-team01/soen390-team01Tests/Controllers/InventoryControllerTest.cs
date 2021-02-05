11f7f1c43947d2a4bdb08e29557da77154d1ad79
bakou912 <tom912_a_@hotmail.com>
2021-02-05 04:13:26 -0500

    adding controller test

---
+++ soen390-team01/soen390-team01Tests/Controllers/InventoryControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+using soen390_team01.Controllers;
+using soen390_team01.Data;
+using soen390_team01.Data.Entities;
+using soen390_team01.Services;
+
+namespace soen390_team01Tests.Services
+{
+    public class InventoryControllerTest
+    {
+        [Test]
+        public void GetAllBikesTest()
+        {
+            var inventoryServiceMock = new Mock<InventoryService>();
+
+            var controller = new InventoryController(inventoryServiceMock.Object);
+        }
+    }
+}
