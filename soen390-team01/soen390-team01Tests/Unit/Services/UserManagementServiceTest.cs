9f5553f8e6cdd4f5e1b135c1bc485696aadbf2ff
Juan Hoyos <juanse.jsh@gmail.com>
2021-02-18 17:37:40 -0500

    User Management Testing

--- soen390-team01/soen390-team01Tests/Unit/Services/UserManagementServiceTest.cs
+++ soen390-team01/soen390-team01Tests/Unit/Services/UserManagementServiceTest.cs
@@ -4,11 +4,7 @@ using NUnit.Framework;
 using soen390_team01.Data;
 using soen390_team01.Data.Entities;
 using soen390_team01.Services;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace soen390_team01Tests.Unit.Services
 {
@@ -16,6 +12,7 @@ namespace soen390_team01Tests.Unit.Services
     {
         private ErpDbContext _context;
         private UserManagementService _userManagementService;
+        private EncryptionService _encryptionService;
 
         [OneTimeSetUp]
         public void Setup()
@@ -23,9 +20,10 @@ namespace soen390_team01Tests.Unit.Services
             var builder = new DbContextOptionsBuilder<ErpDbContext>();
             builder.UseInMemoryDatabase("test_db");
             _context = new ErpDbContext(builder.Options);
+            _encryptionService = new EncryptionService("xg05/WzFW88jHrFxuNGy3vIMC8SMdFBTr/S2r+EPTtY=");
             var authenticationServiceMock = new Mock<AuthenticationFirebaseService>();
             ///key used for testing: xg05/WzFW88jHrFxuNGy3vIMC8SMdFBTr/S2r+EPTtY=
-            _userManagementService = new UserManagementService(_context, new EncryptionService("xg05/WzFW88jHrFxuNGy3vIMC8SMdFBTr/S2r+EPTtY="), authenticationServiceMock.Object);
+            _userManagementService = new UserManagementService(_context, _encryptionService, authenticationServiceMock.Object);
         }
 
         [OneTimeTearDown]
@@ -47,19 +45,7 @@ namespace soen390_team01Tests.Unit.Services
                 LastName = "Doe",
                 Email = "admin@hotmail.com",
                 PhoneNumber = "4385146677",
-                Role = "admin",
-                UserId = 1
-            }));
-
-            // Should be false because the Userid is already used
-            Assert.IsFalse(_userManagementService.AddUser(new User
-            {
-                FirstName = "John",
-                LastName = "Doe",
-                Email = "admin3@hotmail.com",
-                PhoneNumber = "4385146677",
-                Role = "admin",
-                UserId = 1
+                Role = "admin"
             }));
 
             // Should be false because the email should be unique
@@ -69,14 +55,13 @@ namespace soen390_team01Tests.Unit.Services
                 LastName = "Doe",
                 Email = "admin@hotmail.com",
                 PhoneNumber = "4385146677",
-                Role = "admin",
-                UserId = 3
+                Role = "admin"
             }));
 
             Assert.AreEqual(1, _context.Users.ToList().Count);
         }
 
-        [Test]
+        [Test, Order(2)]
         public void GetAllUsersTest()
         {
             Assert.IsTrue(_userManagementService.AddUser(new User
@@ -85,19 +70,42 @@ namespace soen390_team01Tests.Unit.Services
                 LastName = "Doe",
                 Email = "admin2@hotmail.com",
                 PhoneNumber = "4385146677",
-                Role = "admin",
-                UserId = 6
+                Role = "admin"
             }));
             var users = _userManagementService.GetAllUsers();
 
             Assert.IsNotNull(users);
             Assert.IsNotEmpty(users);
-            Assert.AreEqual(1, users.Count);
+            Assert.AreEqual(2, users.Count);
 
             // Checking that the decryption works
-            Assert.IsTrue("John".Equals(users[0].Role));
+            Assert.IsTrue("admin".Equals(users[0].Role));
             Assert.IsTrue("John".Equals(users[0].FirstName));
             Assert.IsTrue("admin2@hotmail.com".Equals(users[1].Email));
+        }
+
+        [Test]
+        public void EditUserTest()
+        {
+            var userToEdit = _userManagementService.GetUserById(_context.Users.ToList()[0].UserId);
+            string newFirstName = "Editing The User";
+            userToEdit.FirstName = newFirstName;
+
+            Assert.IsTrue(_userManagementService.EditUser(userToEdit));
+            
+            var editedUser = _userManagementService.GetUserById(_context.Users.ToList()[0].UserId);
+            Assert.IsTrue(editedUser.FirstName.Equals(newFirstName));
+        }
+
+        [Test]
+        public void GetUserByIdTest()
+        {
+            var userId = _context.Users.ToList()[1].UserId;
+            var user = _userManagementService.GetUserById(userId);
+
+            Assert.NotNull(user);
+            Assert.IsTrue("admin2@hotmail.com".Equals(user.Email));
+            Assert.AreEqual(userId, user.UserId);
         }
     }
 }
