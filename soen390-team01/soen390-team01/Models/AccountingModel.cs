de09c7ec224f5939aeb1c0707f9221e88b50cd0c
Abdalla Osman <>
2021-02-17 22:14:18 -0500

    Accounting view

---
+++ soen390-team01/soen390-team01/Models/AccountingModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using soen390_team01.Data;
+using soen390_team01.Data.Entities;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace soen390_team01.Models
+{
+    public class AccountingModel
+    {
+        public List<Inventory> AllList { get; set; }
+        public List<Inventory> BikeList { get; set; }
+        public List<Inventory> PartList { get; set; }
+        public List<Inventory> MaterialList { get; set; }
+    }
+
+}
