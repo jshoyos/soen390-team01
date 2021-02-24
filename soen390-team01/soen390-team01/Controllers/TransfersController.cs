59aa8e2621a83bf5da2bfda4fcc9b1cf476f134c
abda9725 <49347719+abda9725@users.noreply.github.com>
2021-02-24 17:58:37 -0500

    Transfer view (#67)
    * filter view for transfer view
    
    * Transfer view halfway done
    
    * removed place order button
    
    * took the add procurement button out of the modal
    
    * transfers view completed
    
    Co-authored-by: Sarah Ghorbali <sarahnoor23@gmail.com>
    Co-authored-by: Abdalla Osman <>

---
+++ soen390-team01/soen390-team01/Controllers/TransfersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace soen390_team01.Controllers
+{
+    public class TransfersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
