469f43bdfddb4eda7245334641667053d4d8427f
bakou912 <tom912_a_@hotmail.com>
2021-02-24 23:08:08 -0500

    adding add procurement input min values

--- soen390-team01/soen390-team01/Models/AddProcurementModel.cs
+++ soen390-team01/soen390-team01/Models/AddProcurementModel.cs
@@ -1,19 +1,23 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace soen390_team01.Models
 {
     public class AddProcurementModel
     {
         [Required]
+        [Range(1, long.MaxValue)]
         [Display(Name = "Item ID")]
         public long ItemId { get; set; }
         [Required]
         [Display(Name = "Item Type")]
         public string ItemType{ get; set; }
         [Required]
+        [Range(1, int.MaxValue)]
         [Display(Name = "Item Quantity")]
         public int ItemQuantity { get; set; }
         [Required]
+        [Range(1, long.MaxValue)]
         [Display(Name = "Vendor ID")]
         public long VendorId { get; set; }
     }
