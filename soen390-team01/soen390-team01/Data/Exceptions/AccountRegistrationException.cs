8cfd1a85dea9b5b47e222b5f843b15d1746fe87a
Tommy Andrews <30633066+bakou912@users.noreply.github.com>
2021-02-26 18:41:52 -0500

    Update AccountRegistrationException.cs

--- soen390-team01/soen390-team01/Data/Exceptions/AccountRegistrationException.cs
+++ soen390-team01/soen390-team01/Data/Exceptions/AccountRegistrationException.cs
@@ -1,13 +1,3 @@
-﻿#region Header
-
-// Author: Tommy Andrews
-// File: NullValueException.cs
-// Project: soen390-team01
-// Created: 02/26/2021
-// 
-
-#endregion
-
 namespace soen390_team01.Data.Exceptions
 {
     public class AccountRegistrationException : DataAccessException
