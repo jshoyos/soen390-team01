b294bf15895a677d83ba85de60f569571bbb40e8
unknown <ashphil30@gmail.com>
2021-03-04 00:05:06 -0500

    created accounting model

---
+++ soen390-team01/soen390-team01/Models/AccountingModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using soen390_team01.Data;
+using soen390_team01.Data.Entities;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace soen390_team01.Models
+{
+    public class AccountingModel
+    {
+        public List<Payment> PaymentList { get; set; }
+
+        public string SelectedTab { get; set; } = "All";
+    }
+}
