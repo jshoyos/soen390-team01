39d6acfa2ce2c34f2e87aae130de7f7fbe69a2b7
unknown <ashphil30@gmail.com>
2021-03-07 15:43:29 -0500

    getting new changes

--- soen390-team01/soen390-team01/Models/AccountingModel.cs
+++ soen390-team01/soen390-team01/Models/AccountingModel.cs
@@ -4,13 +4,37 @@ using soen390_team01.Data.Entities;
 using System.Linq;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using soen390_team01.Data.Queries;
 
 namespace soen390_team01.Models
 {
     public class AccountingModel
     {
-        public List<Payment> PaymentList { get; set; }
+        private readonly ErpDbContext _context;
+        public List<Payment> Payments { get; set; }
 
         public string SelectedTab { get; set; } = "All";
+    
+
+    public AccountingModel(ErpDbContext context)
+    {
+        _context = context;
+        
     }
+
+    public AccountingModel SetupModel()
+    {
+        return this;
+    }
+
+    public virtual List<Payment> getPayments()
+        {
+            List<Payment> list = _context.Payments
+                .Include(p => p.P)
+        }
+
+
+    }   
 }
