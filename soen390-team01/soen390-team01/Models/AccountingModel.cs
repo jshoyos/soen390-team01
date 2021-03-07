881a308fde49afeea45f6400daa4e533b3f3031c
Abdalla Osman <a.osman9725@gmail.com>
2021-03-07 12:05:20 -0500

    removed useless lines in csproj and accountingModel

--- soen390-team01/soen390-team01/Models/AccountingModel.cs
+++ soen390-team01/soen390-team01/Models/AccountingModel.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using soen390_team01.Data;
-using soen390_team01.Data.Entities;
-using System.Linq;
-using System.Collections.Generic;
-
-namespace soen390_team01.Models
+﻿namespace soen390_team01.Models
 {
     public class AccountingModel
     {
-        public List<Inventory> AllList { get; set; }
-        public List<Inventory> BikeList { get; set; }
-        public List<Inventory> PartList { get; set; }
-        public List<Inventory> MaterialList { get; set; }
+
     }
 
 }
