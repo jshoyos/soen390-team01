2396e8ff4bc81e8d137d1306ca540de5def91b8c
unknown <ashphil30@gmail.com>
2021-03-09 17:59:43 -0500

    Accounting model + interface

--- soen390-team01/soen390-team01/Models/AccountingModel.cs
+++ soen390-team01/soen390-team01/Models/AccountingModel.cs
@@ -14,27 +14,67 @@ namespace soen390_team01.Models
     {
         private readonly ErpDbContext _context;
         public List<Payment> Payments { get; set; }
-
+        public List<Payment> Receivables { get; set; }
+        public List<Payment> Payables { get; set; }
+        public Filters PaymentFilters { get; set; }
+        public Filters ReceivableFilters { get; set; }
+        public Filters PayableFilters { get; set; }
         public string SelectedTab { get; set; } = "All";
+
+        private static readonly List<string> StatusValues = new List<string> { "Pending", "Completed", "Cancelled" };
     
 
-    public AccountingModel(ErpDbContext context)
-    {
-        _context = context;
-        
-    }
-
-    public AccountingModel SetupModel()
-    {
-        return this;
-    }
-
-    public virtual List<Payment> getPayments()
+        public AccountingModel(ErpDbContext context)
         {
-            List<Payment> list = _context.Payments
-                .Include(p => p.P)
+            _context = context;
+            PaymentFilters = ResetFilters();
+            ReceivableFilters = ResetFilters();
+            PayableFilters = ResetFilters();
         }
 
+        public List<Payment> GetPayments()
+        {
+            List<Payment> list = _context.Payments.ToList();
+            return list;
+        }
+
+        public List<Payment> GetReceivables()
+        {
+            List<Payment> list = _context.Payments.ToList();
+            List<Payment> receivablesList = new List<Payment>();
+            foreach (Payment p in list)
+            {
+                if (p.Amount > 0)
+                {
+                    receivablesList.Add(p);
+                }
+            }
+            return receivablesList;
+        }
+
+        public List<Payment> GetPayables()
+        {
+            List<Payment> list = _context.Payments.ToList();
+            List<Payment> payablesList = new List<Payment>();
+            foreach (Payment p in list)
+            {
+                if (p.Amount < 0)
+                {
+                    payablesList.Add(p);
+                }
+            }
+            return payablesList;
+        }
+
+        private Filters ResetFilters()
+        {
+            var filters = new Filters("payment");
+
+            filters.Add(new CheckboxFilter("payment", "Status", "status", StatusValues));
+            filters.Add(new RangeFilter("payment", "Amount", "amount"));
+
+            return filters;
+        }
 
     }   
 }
---
+++ soen390-team01/soen390-team01/Services/IAccountingService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using soen390_team01.Data.Entities;
+using soen390_team01.Data.Queries;
+
+namespace soen390_team01.Services
+{
+    public interface IAccountingService
+    {
+        public List<Payment> Payments { get; set; }
+        public List<Payment> Receivables { get; set; }
+        public List<Payment> Payables { get; set; }
+        public Filters PaymentFilters { get; set; }
+        public Filters ReceivableFilters { get; set; }
+        public Filters PayableFilters { get; set; }
+        public string SelectedTab { get; set; }
+
+        public List<Payment> GetPayments();
+
+        public List<Payment> GetReceivables();
+
+        public List<Payment> GetPayables();
+    }
+
+}
