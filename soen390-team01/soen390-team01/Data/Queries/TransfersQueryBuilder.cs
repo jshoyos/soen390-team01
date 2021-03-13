9a6e080df61c61d6ab602ee84c40cead524522b5
noahfreger <50425626+noahfreger@users.noreply.github.com>
2021-03-13 11:34:05 -0500

    add

---
+++ soen390-team01/soen390-team01/Data/Queries/TransfersQueryBuilder.cs
@@ -0,0 +1,20 @@
+﻿using soen390_team01.Models;
+using System;
+
+namespace soen390_team01.Data.Queries
+{
+    public static class TransfersQueryBuilder
+    {
+        public static string FilterProcurement(Filters filters)
+        {
+            string query = $"Select procurement.procurement_id, procurement.item_id, procurement.payment_id, procurement.item_quantity, procurement.state, procurement.type, procurement.vendor_id, vendor.name " +
+                   $"From public.procurement, public.vendor where vendor.vendor_id = procurement.vendor_id and {filters.GetConditionsString()}";
+            query = query.Replace("procurement.vendor ", "vendor.name");
+            return query;
+        }
+        public static string FilterOrder(Filters filters)
+        {
+            return $"Select * From public.{filters.Table} where {filters.GetConditionsString()}";
+        }
+    }
+}
---
+++ soen390-team01/soen390-team01/Services/ITransferService.cs
@@ -0,0 +1,36 @@
+﻿using soen390_team01.Data.Entities;
+using soen390_team01.Data.Queries;
+using soen390_team01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace soen390_team01.Services
+{
+    public interface ITransferService
+    {
+
+        public List<Order> Orders { get; set; }
+        public List<Procurement> Procurements { get; set; }
+        public AddProcurementModel AddProcurement { get; set; }
+
+        //Filter list
+        public Filters ProcurementFilters { get; set; } 
+        public Filters OrderFilters { get; set; } 
+
+        public string SelectedTab { get; set; }
+        public bool ShowModal { get; set; }
+
+        public Filters ResetProcurementFilters();
+
+        public Filters ResetOrderFilters();
+
+        public Procurement AddProcurements<T>(AddProcurementModel addProcurement) where T : Item;
+        List<Procurement> GetFilteredProcurementList(Filters filters);
+        List<Procurement> GetProcurements();
+        List<Order> GetFilteredOrderList(Filters filters);
+        List<Order> GetOrders();
+        TransfersModel SetupModel();
+    }
+}
