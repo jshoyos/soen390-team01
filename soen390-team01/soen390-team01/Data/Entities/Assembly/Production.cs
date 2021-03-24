149b76c83cd05f74c6d57bc3bcf6bb9d50f1809c
noahfreger <50425626+noahfreger@users.noreply.github.com>
2021-03-24 14:58:10 -0400

    added production table to db

---
+++ soen390-team01/soen390-team01/Data/Entities/Assembly/Production.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace soen390_team01.Data.Entities
+{
+    public partial class Production
+    {
+        public string State { get; set; }
+        public DateTime Added { get; set; }
+        public DateTime Modified { get; set; }
+        public long BikeId { get; set; }
+        public long ProductionId { get; set; }
+
+        public virtual Bike Bike { get; set; }
+    }
+}
